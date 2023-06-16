628af4a776864980ae108c1b93639dbc988f810f
C.Coutinho <kikijump@gmail.com>
2023-06-16 22:09:08 +0200

    change styles.csv

--- utility.py
+++ utility.py
@@ -275,7 +275,7 @@ def configs_list():
   return [
     'https://github.com/NUROISEA/anime-webui-colab/raw/main/configs/config.json',
     'https://github.com/NUROISEA/anime-webui-colab/raw/main/configs/ui-config.json',
-    'https://github.com/NUROISEA/anime-webui-colab/raw/main/configs/styles.csv',
+    'https://github.com/cc0ut/Stable-Diffusion-Extras/main/utility.py',
   ]
 
 def patch_list():
